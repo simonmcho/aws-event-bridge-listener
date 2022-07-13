b24cbe1e44651a336b2de705090d7dbadeb10284
simonmcho <simonmcho@gmail.com>
2022-07-13 12:26:35 -0700

    Add tsconfig, update port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-const PORT = 8081;
+const PORT = 8080;
 
 app.get("/", (_req, res) => {
   res.send("HELLO! Event Bridge listener running");
